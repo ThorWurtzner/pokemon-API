19d40e70419f3c3c07be547e718e0f3e1b7cff25
Thor Würtzner <6273158@rts-365.dk>
2020-08-27 14:45:08 +0200

    first animation prototype

--- script.js
+++ script.js
@@ -22,14 +22,47 @@ document.querySelector('.pokemonContainer').addEventListener('click', function(e
     fetch("https://pokeapi.co/api/v2/pokemon/" + pokeName)
         .then(response => response.json())
         .then(function(dataSpecifics){
+            console.log(dataSpecifics)
+
             let clonedPokemonTemplate = document.querySelector('#pokemon-specifics').content.cloneNode(true);
             clonedPokemonTemplate.querySelector('.pokemonImg').src = dataSpecifics.sprites.front_default;
             clonedPokemonTemplate.querySelector('.abilityName1').innerText = dataSpecifics.abilities[0].ability.name;
-            clonedPokemonTemplate.querySelector('.abilityName2').innerText = dataSpecifics.abilities[1].ability.name;
+            if (dataSpecifics.abilities[1]) {
+                clonedPokemonTemplate.querySelector('.abilityName2').innerText = dataSpecifics.abilities[1].ability.name;
+            }
+            clonedPokemonTemplate.querySelector('.abilityTitle1').innerText = 'Main ability:'
+            if (!dataSpecifics.abilities[1]) {
+                clonedPokemonTemplate.querySelector('.abilityTitle2').innerText = ''
+            } else {
+                clonedPokemonTemplate.querySelector('.abilityTitle2').innerText = 'Secondary ability:'
+            }
             
-            event.target.appendChild(clonedPokemonTemplate);
+            fetch(dataSpecifics.location_area_encounters)
+                .then(response => response.json())
+                .then(function(dataLocation){
+                    console.log(dataLocation);
 
-            console.log(dataSpecifics)
+                    if (dataLocation[0]) {
+                        clonedPokemonTemplate.querySelector('.location-wrapper').classList.add('location-wrapper-style');
+                        clonedPokemonTemplate.querySelector('.locationTitle').innerText = 'Found in:';
+                        clonedPokemonTemplate.querySelector('.location1').innerText = dataLocation[0].location_area.name
+                        clonedPokemonTemplate.querySelector('.location1').classList.add('location-style');
+                    }
+                    if (dataLocation[1]) {
+                        clonedPokemonTemplate.querySelector('.location2').innerText = dataLocation[1].location_area.name
+                        clonedPokemonTemplate.querySelector('.location2').classList.add('location-style');
+                    }
+                    if (dataLocation[2]) {
+                        clonedPokemonTemplate.querySelector('.location3').innerText = dataLocation[2].location_area.name
+                        clonedPokemonTemplate.querySelector('.location3').classList.add('location-style');
+                    }
+                    event.target.classList.remove('expand-away');
+                    event.target.classList.add('expand')
+                    setTimeout(() => {
+                        event.target.appendChild(clonedPokemonTemplate);
+                    }, 400);
+                });
+
         });
 })
 
@@ -39,10 +72,13 @@ document.querySelector('.pokemonContainer').addEventListener('click', function(e
     pokemonWrappers.forEach(pokemonWrapper => {
         if (pokemonWrapper.contains(pokemonSpecifics)) {
             console.log(event.target);
-            pokemonSpecifics.classList.add('slide-away')
-            setTimeout(() => {
-                pokemonWrapper.removeChild(pokemonSpecifics);
-            }, 400);
+            event.target.classList.remove('expand');
+            // pokemonSpecifics.classList.add('slide-away')
+            event.target.classList.add('expand-away');
+            pokemonWrapper.removeChild(pokemonSpecifics);
+            // setTimeout(() => {
+
+            // }, 400);
         }
     });
 })
